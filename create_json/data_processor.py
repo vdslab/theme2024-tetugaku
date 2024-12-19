59a1bef83a33ece804e5a5d8c7d7414444445ce6
sakamoti55 <kohseimeimei@gmail.com>
2024-12-20 04:25:31 +0900

    全てのデータ加工が行われるように機能修正

--- create_json/data_processor.py
+++ create_json/data_processor.py
@@ -1,4 +1,5 @@
 import json
+import ast
 
 processed_file = "./output_data/processed_data.json"
 
@@ -20,22 +21,73 @@ def process_json(data,file):
     }
 
     for row in rows:
+        # headersとrowを結び付ける
         row_dict = dict(zip(headers, row))
-        
-        # 1. name
+
+        # 文字列から配列に変更
+        keywords = [item.strip().strip("\"") for item in row_dict["keywords"].split(",")]
+        target_ids = ast.literal_eval(row_dict["target_id"])
+        relations = ast.literal_eval(row_dict["relations"])
+
+        # 文字列を数値に変換
+        name_id = int(row_dict["name_id"])
+        group_id = int(row_dict["group_id"])
+
+        fixed_targets_ids = []
+
+        fixed_targets_ids.extend([
+            int(target)
+            for target in target_ids
+        ])
+
+        # namesの作成
         result["names"].append({
-            "name_id": row_dict["name_id"],
+            "name_id": name_id,
             "name": row_dict["name"]
         })
     
-    with open(file, 'w' ,encoding='utf-8') as file:
-        json.dump(result, file, ensure_ascii=False, indent=4)  
+
+
+
+        # nodesの作成
+        result["nodes"].append({
+            "name_id": name_id,
+            "group_id": group_id,
+            "group_name": row_dict["group_name"],
+            "information": row_dict["information"]
+        })
+
+        # linksの作成
+        result["links"].extend([
+            {
+                "name_id" : name_id,
+                "target_id" : fixed_targets_ids[i],
+                "relation_id" : relations[i]
+            }
+            for i in range(len(fixed_targets_ids))
+        ])
+
+        # keywordsの作成
+        result["keywords"].extend([
+            {
+                "name_id" : name_id,
+                "keyword": keyword
+            }
+            for keyword in keywords
+        ])
+
+        # これと同じ 
+        # for keyword in keywords:
+        #     result["keywords"].append({
+        #         "name_id" : name_id,
+        #         "keyword": keyword        
+        # })
+
+    # データをファイルに保存
+    with open(file, 'w', encoding='utf-8') as file:
+        json.dump(result, file, ensure_ascii=False, indent=4)
 
     return result
 
 data = fetch_data()
-
 process_json(data,processed_file)
-
-
-
