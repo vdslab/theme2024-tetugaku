6adfbf8b0ba005dbaceaef4f5e40a26f5d70ae7e
sakamoti55 <kohseimeimei@gmail.com>
2024-12-22 04:12:59 +0900

    インデント修正

--- src/App.jsx
+++ src/App.jsx
@@ -10,38 +10,38 @@ function App() {
     setClickedNodeId(newNodeId);
   };
 
-    return (
-      <div className="wrapper">
-        {/* wrapper-styles.css適用箇所 */}
-        <header><h1>ヘッダー</h1></header>
-        <main>
-          {/* main-styles.css適用箇所 */}
-          <div className="full-container">
-            <div className="select-container">
-              <h1>セレクトボタン</h1>
-            </div>
-            <div className="item-container">
-              {/* item-styles.css適用箇所 */}
-              <div className="info-list-container">
-                <div className="info-container">
-                <h1>思想家の詳細</h1>
+  return (
+    <div className="wrapper">
+      {/* wrapper-styles.css適用箇所 */}
+      <header><h1>ヘッダー</h1></header>
+      <main>
+        {/* main-styles.css適用箇所 */}
+        <div className="full-container">
+          <div className="select-container">
+            <h1>セレクトボタン</h1>
+          </div>
+          <div className="item-container">
+            {/* item-styles.css適用箇所 */}
+            <div className="info-list-container">
+              <div className="info-container">
+              <h1>思想家の詳細</h1>
               <Information processed_data={processed_data} nodeId={clickedNodeId}/>
-                </div>
-                <div className="list-container">
-                <h1>リストの表示</h1>
-                </div>
               </div>
-              <div className="vis-container">
+              <div className="list-container">
+              <h1>リストの表示</h1>
+              </div>
+            </div>
+            <div className="vis-container">
               <NetworkGraph processed_data={processed_data} onNodeClick={handleNodeClick}/>
-              </div>
-              <div className="book-container">
-              <h1>本棚</h1>
-              </div>
+            </div>
+            <div className="book-container">
+            <h1>本棚</h1>
             </div>
           </div>
-        </main>
-      </div>
-    );
-  }
-  
-  export default App;
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default App;
