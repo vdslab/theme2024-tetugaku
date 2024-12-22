57bd936a2112f417a9c80a42ca81017ddff57b87
sakamoti55 <kohseimeimei@gmail.com>
2024-12-22 19:32:00 +0900

    info-mainの追加 + informationComponentの大きさを親要素に適合

--- src/components/Information.jsx
+++ src/components/Information.jsx
@@ -2,7 +2,7 @@ function Information({ processed_data, nodeId }) {
     const nodeName = processed_data.names.find(e => e.name_id === nodeId)
 
 return (
-      <div>
+      <div className="info-main">
         <h1>{nodeId}</h1>
         <h2>{nodeName ? nodeName.name : '該当するデータがありません'}</h2>
       </div>
