edc6eed5672cc19af36ed3969c6089d7a65e99ba
wwatanabe331 <wwatanabe331@gmail.com>
2025-01-15 17:41:17 +0900

    一旦

--- src/components/NetWorkGraph.jsx
+++ src/components/NetWorkGraph.jsx
@@ -1,7 +1,24 @@
-import { useEffect, useState ,useRef} from "react";
-import * as d3 from 'd3';
+import { useEffect, useState, useRef } from "react";
+import * as d3 from "d3";
 
- useEffect(() => {
+function NetworkGraph({ processed_data, onNodeClick, selectedNodeId }) {
+  const [data, setData] = useState(null);
+  const svgRef = useRef(null);
+
+  // useRefにズーム用インスタンスを保存
+  const zoomInstance = useRef(
+    d3.zoom().on("zoom", (event) => {
+      // "g" 要素に対して transform を適用
+      d3.select(svgRef.current)
+        .select("g.main-group")
+        .attr("transform", event.transform);
+    })
+  );
+
+  // 隣接リストの準備
+  const nearestNodeList = useRef({});
+
+  useEffect(() => {
     setData(processed_data);
 
     if (!data) return;
@@ -20,7 +37,6 @@ import * as d3 from 'd3';
       const toCenter = d3.zoomIdentity
         .translate(250 - x * 2, 250 - y * 2)
         .scale(2);
-      //クリックした時のzoomレベルをあげたい
 
       svg
         .transition()
@@ -28,75 +44,41 @@ import * as d3 from 'd3';
         .call(zoomInstance.current.transform, toCenter);
     };
 
-function NetworkGraph({processed_data,onNodeClick,selectedNodeId}) {
-    const [data,setData] = useState(null);
-    const svgRef = useRef(null);
+    // ノードクリックでノードとエッジをハイライト表示
+    const highlightByNodeClick = (e, clickedNode) => {
+      const nearests = nearestNodeList.current[clickedNode.id];
 
-    // useRefにズーム用インスタンスを保存
-    const zoomInstance = useRef(
-      d3.zoom().on("zoom", (event) => {
-        // "g" 要素に対して transform を適用
-        d3.select(svgRef.current).select("g.main-group").attr("transform", event.transform);
-      })
-    );
+      node
+        .transition()
+        .duration(300)
+        .attr("fill-opacity", (d) => {
+          return d.id === clickedNode.id || nearests.has(d.id) ? 1 : 0.5;
+        });
 
-    // 隣接リストの準備
-    const nearestNodeList = useRef({});
+      link
+        .transition()
+        .duration(300)
+        .attr("stroke-opacity", (l) => {
+          const sourceId =
+            typeof l.source === "object" ? l.source.id : l.source;
+          const targetId =
+            typeof l.target === "object" ? l.target.id : l.target;
 
-    useEffect(() => {
-        setData(processed_data);
+          return sourceId === clickedNode.id || targetId === clickedNode.id
+            ? 1
+            : 0.1;
+        });
+    };
 
-        if(!data)return;
+    const svg = d3
+      .select(svgRef.current)
+      // viewBox(minx,miny,w,h) w,hは初期描画範囲の設計
+      // -> 値が大きいほどグラフも大きくなる
+      .attr("viewBox", `0 0 500 500`)
+      .attr("preserveAspectRatio", "xMidYMid meet")
+      .call(zoomInstance.current);
 
-        // ノードクリックでonNodeClickを実行
-        const selectByNodeClick = (e,clickedNode) => {
-            onNodeClick(clickedNode.id);
-        };
-
-        // ノードクリックでノードを中心へ移動
-        const zoomByNodeClick = (e,clickedNode) => {
-            const x = clickedNode.x;
-            const y = clickedNode.y;
-
-            // ノードを中心へ移動   
-            const toCenter = d3.zoomIdentity
-              .translate(250 - x, 250 - y)
-
-            svg.transition()
-               .duration(750)  
-               .call(zoomInstance.current.transform, toCenter);
-        }
-
-        // ノードクリックでノードとエッジをハイライト表示
-        const highlightByNodeClick = (e,clickedNode) => {
-            const nearests = nearestNodeList.current[clickedNode.id];
-
-            node
-              .transition()
-              .duration(300)
-              .attr("fill-opacity", (d) => {
-                return d.id === clickedNode.id || nearests.has(d.id) ? 1 : 0.5;
-            });
-        
-            link
-              .transition()
-              .duration(300)
-              .attr("stroke-opacity", (l) => {
-                const sourceId = typeof l.source === "object" ? l.source.id : l.source;
-                const targetId = typeof l.target === "object" ? l.target.id : l.target;
-
-                return sourceId === clickedNode.id || targetId === clickedNode.id ? 1 : 0.1;
-            });
-        }
-
-        const svg = d3.select(svgRef.current)
-            // viewBox(minx,miny,w,h) w,hは初期描画範囲の設計　
-            // -> 値が大きいほどグラフも大きくなる 
-            .attr("viewBox", `0 0 500 500`)
-            .attr("preserveAspectRatio", "xMidYMid meet")
-            .call(zoomInstance.current);
-
-        const svgGroup = svg.append("g").attr("class", "main-group");
+    const svgGroup = svg.append("g").attr("class", "main-group");
 
     // scaleOrdinalの引数は色範囲
     // 後程でるgroupの個数がドメイン
@@ -161,8 +143,7 @@ function NetworkGraph({processed_data,onNodeClick,selectedNodeId}) {
           return d.id;
         })
       )
-      // ノードの距離を縮めたい
-      .force("charge", d3.forceManyBody(0).strength(-30))
+      .force("charge", d3.forceManyBody(0).strength(-100))
       .force("center", d3.forceCenter(250, 250));
 
     // シミュレーション
@@ -198,18 +179,21 @@ function NetworkGraph({processed_data,onNodeClick,selectedNodeId}) {
       d3.select(svgRef.current).selectAll("*").remove();
     };
   }, [data]);
-  
+
   // selectedNodeIdを持つノードを中心に移動
   useEffect(() => {
-      if (!data || !selectedNodeId) return
-      const selectedNode = data.nodes.find((node) => node.id === selectedNodeId);
-      if (selectedNode) {
-          const x = selectedNode.x;
-          const y = selectedNode.y
-          const toCenter = d3.zoomIdentity.translate(250 - x, 250 - y)
-          d3.select(svgRef.current).transition().duration(750).call(zoomInstance.current.transform, toCenter);
-      }
-  }, [selectedNodeId, data]);  
+    if (!data || !selectedNodeId) return;
+    const selectedNode = data.nodes.find((node) => node.id === selectedNodeId);
+    if (selectedNode) {
+      const x = selectedNode.x;
+      const y = selectedNode.y;
+      const toCenter = d3.zoomIdentity.translate(250 - x, 250 - y);
+      d3.select(svgRef.current)
+        .transition()
+        .duration(750)
+        .call(zoomInstance.current.transform, toCenter);
+    }
+  }, [selectedNodeId, data]);
 
   return <svg ref={svgRef} className="network-graph"></svg>;
 }
