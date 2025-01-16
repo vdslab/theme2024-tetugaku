959b4a7809c1c27b9adbbed93946609b3939ff8c
sakamoti55 <kohseimeimei@gmail.com>
2025-01-16 10:16:31 +0900

    groupを選択するセレクトボタンの作成

--- src/components/SearchAndFilter.jsx
+++ src/components/SearchAndFilter.jsx
@@ -6,11 +6,15 @@ function SearchAndFilter({ processed_data, selectNode }) {
   const [selectedPhilosopher, setSelectedPhilosopher] = useState(null);
   const [key, setKey] = useState(0); // 再レンダリング用のkeyを管理
 
+  const [selectedGroup, setSelectedGroup] = useState(null);
+
+  //思想家選択用オプション
   const options = processed_data.names.map((item) => ({
     value: item.name,
     label: item.name,
   }));
 
+  //思想家選択用イベントハンドラ
   const handleSelectChange = (selectedOption) => {
     if (selectedOption) {
       setSelectedPhilosopher(selectedOption);
@@ -29,7 +33,7 @@ function SearchAndFilter({ processed_data, selectNode }) {
     }
     // inputValueが空欄になったが、メニューを閉じていない状態(文字を削除しただけの状態)
     if (action !== 'menu-close' && inputValue === '') {
-      setSelectedPhilosopher(null);  // 不要な場合はコメントアウト
+      setSelectedPhilosopher(null);  // 不要な場合はコメントアウト  
     }
     // メニューを閉じた時に選択がなければプレースホルダーを表示する
     if (action === "menu-close" && !selectedPhilosopher) {
@@ -37,6 +41,31 @@ function SearchAndFilter({ processed_data, selectNode }) {
     }
   };
 
+
+  //グループ選択用オプション
+  const groupMap = new Map();
+  processed_data.nodes.forEach((node) => {
+    if (!groupMap.has(node.group)) {
+      groupMap.set(node.group, node.group_name);
+    }
+  });
+
+  // Mapから { value, label } の形式に変換
+  const groupOptions = Array.from(groupMap).map(([grp, grpName]) => ({
+    value: grp,
+    label: grpName, // group_name をラベルに表示
+  }));
+
+  //グループ選択用イベントハンドラ
+  const handleGroupChange = (selectedOption) => {
+    if (selectedOption) {
+      setSelectedGroup(selectedOption);
+    } else {
+      setSelectedGroup(null);
+    }
+  };
+
+  // スタイル
   const customStyles = {
     container: (provided) => ({
       ...provided,
@@ -53,32 +82,45 @@ function SearchAndFilter({ processed_data, selectNode }) {
   };
 
   useEffect(() => {
-    // nullの処理
-    if(!selectedPhilosopher)return; 
+    if(!selectedPhilosopher)return;
     processed_data.names.forEach(t => {
       if (t.name === selectedPhilosopher.value) {
         selectNode(t.name_id);
       }
     });
   }, [selectedPhilosopher]);
-  
+
   return (
-    <div>
-      <Select
-        // keyを変更して再マウント
-        key={key}
-        // 選択オプション
-        options={options}
-        // 
-        value={selectedPhilosopher}
-        onChange={handleSelectChange}
-        onInputChange={handleInputChange}
-        inputValue={searchName}
-        placeholder="思想家を選んでください"
-        isClearable
-        styles={customStyles}
-      />
-    </div>
+    <>
+      {/* 思想家を選択 */}
+      <div>
+        <Select
+          // keyを変更して再マウント
+          key={key}
+          options={options}
+          value={selectedPhilosopher}
+          onChange={handleSelectChange}
+          onInputChange={handleInputChange}
+          inputValue={searchName}
+          placeholder="思想家を選んでください"
+          isClearable
+          styles={customStyles}
+        />
+      </div>
+
+      {/* グループを選択 */}
+      <div>
+        <Select
+          options={groupOptions}
+          value={selectedGroup}
+          onChange={handleGroupChange}
+          placeholder="グループを選んでください"
+          isClearable
+          isSearchable={false}
+          styles={customStyles}
+        />
+      </div>
+    </>
   );
 }
 
