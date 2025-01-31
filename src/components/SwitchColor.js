2fc20e05a41f6fb7301eccafb32273bcd8a1ba98
wwatanabe331 <wwatanabe331@gmail.com>
2025-01-31 17:00:00 +0900

    一旦

--- src/components/SwitchColor.js
+++ src/components/SwitchColor.js
@@ -2,37 +2,37 @@ import { styled } from "@mui/material/styles";
 import { Switch } from "@mui/material"; // MUI Switch
 
 export const RedSwitch = styled(Switch)(({ theme }) => ({
-    "& .MuiSwitch-switchBase.Mui-checked": {
-      color: "red",                         // つまみの色
-    },
-    "& .MuiSwitch-switchBase.Mui-checked + .MuiSwitch-track": {
-      backgroundColor: "red",               // つまみの下の色
-    },
-  }));
-  
-  export const BlueSwitch = styled(Switch)(({ theme }) => ({
-    "& .MuiSwitch-switchBase.Mui-checked": {
-      color: "blue",
-    },
-    "& .MuiSwitch-switchBase.Mui-checked + .MuiSwitch-track": {
-      backgroundColor: "blue",
-    },
-  }));
-  
-  export const GreenSwitch = styled(Switch)(({ theme }) => ({
-    "& .MuiSwitch-switchBase.Mui-checked": {
-      color: "green",
-    },
-    "& .MuiSwitch-switchBase.Mui-checked + .MuiSwitch-track": {
-      backgroundColor: "green",
-    },
-  }));
-  
-  export const OrangeSwitch = styled(Switch)(({ theme }) => ({
-    "& .MuiSwitch-switchBase.Mui-checked": {
-      color: "orange",
-    },
-    "& .MuiSwitch-switchBase.Mui-checked + .MuiSwitch-track": {
-      backgroundColor: "orange",
-    },
-  }));
+  "& .MuiSwitch-switchBase.Mui-checked": {
+    color: "#82BC8A", // つまみの色
+  },
+  "& .MuiSwitch-switchBase.Mui-checked + .MuiSwitch-track": {
+    backgroundColor: "#9FCBA5", // つまみの下の色
+  },
+}));
+
+export const BlueSwitch = styled(Switch)(({ theme }) => ({
+  "& .MuiSwitch-switchBase.Mui-checked": {
+    color: "#D98050",
+  },
+  "& .MuiSwitch-switchBase.Mui-checked + .MuiSwitch-track": {
+    backgroundColor: "#E29E7A",
+  },
+}));
+
+export const GreenSwitch = styled(Switch)(({ theme }) => ({
+  "& .MuiSwitch-switchBase.Mui-checked": {
+    color: "#D06098",
+  },
+  "& .MuiSwitch-switchBase.Mui-checked + .MuiSwitch-track": {
+    backgroundColor: "#DB85B1",
+  },
+}));
+
+export const OrangeSwitch = styled(Switch)(({ theme }) => ({
+  "& .MuiSwitch-switchBase.Mui-checked": {
+    color: "#5B43FF",
+  },
+  "& .MuiSwitch-switchBase.Mui-checked + .MuiSwitch-track": {
+    backgroundColor: "#8674FF",
+  },
+}));
