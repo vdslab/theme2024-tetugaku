52fdbfa8716b6dd7d494a07b4ecbd6da2dd45f0f
sakamoti55 <kohseimeimei@gmail.com>
2025-02-16 06:44:43 +0900

    AppFMのみでrouterが使用されるように修正

--- src/AppContainer.jsx
+++ src/AppContainer.jsx
@@ -1,11 +1,14 @@
 import { useState, useEffect } from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App";
 import AppFM from "./AppFM";
+import { routes } from "./rooterFM/routes.jsx"; // routes.jsをimport
 
 function AppContainer() {
   const [isMobile, setIsMobile] = useState(
-    window.innerWidth / window.innerHeight < 1,
+    window.innerWidth / window.innerHeight < 1
   );
+
   useEffect(() => {
     const handleResize = () => {
       setIsMobile(window.innerWidth / window.innerHeight < 1);
@@ -14,7 +17,15 @@ function AppContainer() {
     return () => window.removeEventListener("resize", handleResize);
   }, []);
 
-  return isMobile ? <AppFM /> : <App />;
+  const router = createBrowserRouter(routes); // ルーター作成
+
+  return isMobile ? (
+    <RouterProvider router={router}>
+      <AppFM />
+    </RouterProvider>
+  ) : (
+    <App />
+  );
 }
 
 export default AppContainer;
