01300bbc81d101cf16b149f08813f67246c1e348
sakamoti55 <kohseimeimei@gmail.com>
2025-02-17 04:39:04 +0900

    Home追加

--- src/Mobile/router/routes.jsx
+++ src/Mobile/router/routes.jsx
@@ -1,15 +1,13 @@
 import { Navigate } from "react-router-dom";
-import RootLayouts from "../components/layouts/RootLayout.jsx";
-import MainLayouts from "../components/layouts/MainLayout.jsx";
-import Loading from "../components/pages/Loading/Loading.jsx";
-import BottomSheet from "../components/pages/Main/BottomSheet.jsx";
+import RootLayouts from "@m/components/layouts/RootLayout.jsx";
+import MainLayouts from "@m/components/layouts/MainLayout.jsx";
+import Loading from "@m/components/pages/Loading/Loading.jsx";
+import Home from "@m/components/pages/Main/Home.jsx";
 
 // ルート定義を簡略化
 const route = (path, element, children = []) => ({ path, element, children });
 
-const mainRoutes = [
-  route("/", <MainLayouts />, [route("sheet", <BottomSheet />)]),
-];
+const mainRoutes = [route("/", <MainLayouts />, [route("sheet", <Home />)])];
 
 export const routes = [
   route("/", <RootLayouts />, [
