2f45aac597cf12a3aee1328ca90b62260c2d9ca7
sakamoti55 <kohseimeimei@gmail.com>
2025-02-17 09:02:43 +0900

    スクロールの仕様を修正

--- src/Mobile/components/organisms/information/Information.jsx
+++ src/Mobile/components/organisms/information/Information.jsx
@@ -1,10 +1,33 @@
 import InformationCard from "./InformationCard.jsx";
 import "./Information.css";
+import { useRef } from "react";
 
-export default function Information({ data, nodeId }) {
+export default function Information({
+  data,
+  nodeId,
+  sheetY,
+  openPosition,
+  onScrollToClose,
+}) {
+  const containerRef = useRef(null);
+
+  const handleScroll = () => {
+    if (containerRef.current.scrollTop === 0) {
+      onScrollToClose(); // 最上部に達したらスムーズに閉じる
+    }
+  };
+
   return (
-    <div className="information-container">
-      <div className="information-header">解説</div>
+    <div
+      ref={containerRef}
+      className="information-container-fm"
+      style={{
+        overflowY: sheetY === openPosition ? "auto" : "hidden",
+        scrollBehavior: "smooth",
+      }}
+      onScroll={handleScroll}
+    >
+      <div className="information-header-fm">解説</div>
       <InformationCard data={data} nodeId={nodeId} />
     </div>
   );
